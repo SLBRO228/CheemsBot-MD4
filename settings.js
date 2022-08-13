268e8409683bdd8e83d2be68efb585940a6ee01e
SLBRO228 <107413248+SLBRO228@users.noreply.github.com>
2022-08-13 11:37:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -61,11 +61,11 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['94776297506']
+global.ownertag = '94776297506' //ur tag number
+global.botname = 'mg official bot' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.websitex = "www.whatsapp.com" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
